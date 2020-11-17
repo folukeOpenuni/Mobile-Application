447d72f9ca9e23cbbfebf3ee989176118120f30b
foluke agbede <folukeagbede@icloud.com>
2020-11-17 22:23:48 +0000

    added text view

---
+++ honeycombworks/app/src/main/java/com/thehoneycombworks/DatabaseOpenHelper.java
@@ -0,0 +1,28 @@
+package com.thehoneycombworks;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1; // Database Version
+
+    private static final String DATABASE_NAME = "customer_questions_db"; // Database Name
+
+    //constructor
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
