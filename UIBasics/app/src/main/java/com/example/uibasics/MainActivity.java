569377c9753610872e6c67f6efb2fd19e5527217
foluke agbede <folukeagbede@icloud.com>
2020-11-17 20:19:18 +0000

    added second ui element

---
+++ UIBasics/app/src/main/java/com/example/uibasics/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.uibasics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onHelloBtnClicked(View view){
+        TextView txtWelcome = findViewById(R.id.txtWelcome);
+        txtWelcome.setText("Hello Again");
+    }
+}
